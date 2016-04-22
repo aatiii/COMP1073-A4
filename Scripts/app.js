c862904413b794ce5d2b5ba4706b8d59e5e24457
aatiii <helen_ati@163.com>
2016-04-21 21:32:51 -0400

    files uploaded

---
+++ Scripts/app.js
@@ -0,0 +1,79 @@
+"use strict";
+
+// setup IIFE
+
+
+(function () {
+
+
+    var replaceBio = function() {
+        // declare variable
+        var bio;
+
+        bio = document.getElementById("bio");
+        bio.innerHTML = "I'm Hailun Li, an international student studying Web design and development. " +
+        " This is my fourth year in Ontario, Canada. " +
+        " I was previously studying Cognitive science and Computer science. I have some experience"+
+        " with programming languages such as python, java, html, php, etc";
+    }
+
+    // execute the function
+    replaceBio();
+
+    // createjs section
+
+    //reference to canvas element
+    var canvas = document.getElementById('canvas');
+    // canvas.setAttribute("position", screenWidth);
+    $('#canvas').css('position', 'fixed');
+    $('#canvas').css('bottom', '0');
+    $('#canvas').css('left', '0');
+    canvas.setAttribute("height", "250");
+    canvas.setAttribute("width", "250");
+
+    // create a stage container object
+    var stage = new createjs.Stage(canvas);
+
+    var text = null
+    text.x = 30;
+    text.y = 450;
+
+    var image = null;
+    image.x = 30;
+    image.y = 450;
+
+    function init() {
+      stage.enableMouseOver(20);
+      createjs.Ticker.setFPS(80);
+      createjs.Ticker.addEventListener("tick", animation)
+
+      main();
+    }
+
+    function animation() {
+      // move the object from side to side
+      createjs.Tween.get(text, { loop: true })
+        .to({x:130,y:450},2000,createjs.Ease.getPowIn(2.5));
+        .to({x:30,y:450},2000,createjs.Ease.getPowIn(2.5));
+      stage.update();
+
+      createjs.Tween.get(image, { loop: true })
+        .to({x:130,y:450},2000,createjs.Ease.getPowIn(2.5));
+        .to({x:30,y:450},2000,createjs.Ease.getPowIn(2.5));
+      stage.update();
+    }
+
+    function main() {
+        text = new createjs.Text("Check Out My Project", "50px Tahoma", "#eee");
+        stage.addChild(text);
+
+        image = new createjs.Bitmap('../Content/p1.jpg');
+        stage.addChild(image);
+        // redirect user to project page
+        image.on("click", function() {
+          window.location.href = "../project.html"
+        })
+    }
+
+    window.onload = init;
+})();
---
+++ Scripts/project.js
@@ -0,0 +1,46 @@
+"use strict";
+
+// project 1 text replacement
+
+(function() {
+    
+  var replaceProject1 = function() {
+      
+      var project1;
+      
+      project1 = document.getElementById("project1");
+      project1.innerHTML="This is a project about web page design. I designed the home page of my personal website. The images in the cover flow at the top and the content part at the bottom provide the web page with a reflective horizontal symmetrical balance. The white space in the middle functioned as a horizontal axis line. The division of the photos as well as the content at the bottom generally follows the rule of thirds. The drop shadows of the photos direct to the bottom in order to draw the audiences’ attention to the content below the photos. The color scheme of the website is generally monochromatic with a light greenish blue. The overall color scheme sets the website with a mood of calm and clean.";
+  }
+  replaceProject1();    
+  
+})();
+
+// project 2 text replacement
+
+(function() {
+    
+  var replaceProject2 = function() {
+      
+      var project2;
+      
+      project2 = document.getElementById("project2");
+      project2.innerHTML="This project simulates the commercial page for a record player store. Users are able to browse the products, order and purchase the products, and check the contact information of the manufacturer.";
+  }
+  replaceProject2();    
+  
+})();
+
+// project 3 text replacement
+
+(function() {
+    
+  var replaceProject3 = function() {
+      
+      var project3;
+      
+      project3 = document.getElementById("project3");
+      project3.innerHTML="This is a project that illustrates the information architecture of a simulated website. A well-designed information architecture would increase the functionality of the website. ";
+  }
+  replaceProject3();    
+  
+})();
